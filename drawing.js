60aa6c994164cda3491937fc0b04e20508df0474
Zaida Kodzoeva <zkod777@gmail.com>
2022-03-03 21:25:24 +0400

    Attempt at adding a border to the canvas

---
+++ drawing.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(800, 800); 
+    fill(255,182,193);
+    strokeWeight(2);
+    stroke(51);
+    rect(2, 2, 800-2, 800-2);
+    noStroke();
+  }
+  
+  function draw() {
+    
+    if (mouseIsPressed) {
+      fill(0);
+    } else{
+        noFill();
+    }
+    
+    ellipse(mouseX, mouseY, 3, 3);
+  }
--- p5.js-exploration.js
+++ p5.js-exploration.js
@@ -1,12 +1,16 @@
 function setup() {
   createCanvas(800, 800);  
+  background(100);
+  stroke(200);
+  // strokeWeight(2);
+  //stroke(20);
+  // rect(2, 2, 800-2, 800-2);
 }
 ////make a way for ppl to open a pic and then trace it
 
 function draw() {
   if (mouseIsPressed) {
     fill(0);
-    OfflineAudioCompletionEvent()
   } else {
     fill(255);
   }
