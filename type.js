d6e3ffbe255d45eee33654bdd016738c12032208
Zaida Kodzoeva <zkod777@gmail.com>
2022-03-05 00:22:01 +0400

    Attempt at letting users type on screen

---
+++ type.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800, 800);
+    textSize(90);
+    fill(255);
+  }
+  function draw() {
+    background(0);
+    text(key, 150, 275); // Draw at coordinate (20,75)
+
+    line(1,0,4,5);
+  }
